6ca9bafbf5ef05816ee81a61f7410374d266d901
Denis <d.limarev@corp.mail.ru>
2021-12-19 01:26:12 +0300

    add pre-commit hook

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+var _ = fmt.Sprintf("123") // rule match example
 
 func main() {
 	log.Printf("on server: %s", http.ListenAndServe(":80", nil))
